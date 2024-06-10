ae1dc0da96b9e76844c39376202f4464f2955148
hieptvh18 <hieptvh18@gmail.com>
2024-06-10 22:10:43 +0700

    update base code

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // app.js
 const express = require('express');
-const { sequelize, User } = require('./src/configs/database');
+require('dotenv').config(); // load all env variable
 
 const app = express();
-const port = 3000;
+const port = process.env.SERVER_PORT;
 
 // Middleware to parse JSON requests
 app.use(express.json());
@@ -11,27 +11,6 @@ app.use(express.json());
 // Define routes
 app.get('/', (req, res) => {
   res.send('Hello World!');
-});
-
-// Create a new user
-app.post('/users', async (req, res) => {
-  try {
-    const { username, email } = req.body;
-    const newUser = await User.create({ username, email });
-    res.status(201).json(newUser);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-// Get all users
-app.get('/users', async (req, res) => {
-  try {
-    const users = await User.findAll();
-    res.status(200).json(users);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
 });
 
 // Start the server
--- config/database.js
+++ config/database.js
@@ -1,10 +1,15 @@
-{
+require('dotenv').config(); 
+
+console.log('DATABASE_USERNAME:', process.env.DATABASE_USERNAME);  
+
+module.exports = {
   "development": {
-    "username": process.env.DATABASE_USER,
+    "username": process.env.DATABASE_USERNAME,
     "password": process.env.DATABASE_PASSWORD,
     "database": process.env.DATABASE_NAME,
     "host": process.env.DATABASE_HOST,
-    "dialect": process.env.DATABASE_CONNECTION
+    "dialect": process.env.DATABASE_CONNECTION,
+    logging: console.log,
   },
   "test": {
     "username": "root",
